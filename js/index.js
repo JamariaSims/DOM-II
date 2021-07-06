a8fca5218d42cc9cc2e11f17f07a33268c6bd617
Jamaria Sims <75871993+UnloadedZero@users.noreply.github.com>
2021-07-06 19:49:09 -0400

    Done!

--- js/index.js
+++ js/index.js
@@ -1,18 +1,29 @@
 // Global Variables
 var x = 7;
-var times = 0;
+var timesUserPressesEsc = 0;
 var colors = ["red", "green", "yellow", "purple", "blue"];
+var events = [
+	"mouseover",
+	"mouseout",
+	"click",
+	"wheel",
+	"dblclick",
+	"keyup",
+	"load",
+	"keydown",
+];
 //Buttons Area
 const groupButtons = document.querySelectorAll("a");
 groupButtons.forEach((button) => {
-	console.log(button);
-	button.addEventListener("mouseover", (e) => {
+	button.addEventListener(events[0], (e) => {
 		e.target.style.color = "red";
+		e.target.style.padding = "4%";
 	});
-	button.addEventListener("mouseout", (e) => {
+	button.addEventListener(events[1], (e) => {
 		e.target.style.color = "black";
+		e.target.style.padding = "0%";
 	});
-	button.addEventListener("click", (e) => {
+	button.addEventListener(events[2], (e) => {
 		setTimeout(() => {
 			button.style.color = "green";
 			preventDefault();
@@ -20,26 +31,32 @@ groupButtons.forEach((button) => {
 	});
 });
 //Page Area
-document.addEventListener("wheel", (e) => {
+document.addEventListener(events[3], (e) => {
 	groupButtons.forEach((button) => {
 		button.style.backgroundColor = "yellow";
 		button.style.padding = "4%";
 		setTimeout(() => {
-			button.style.backgroundColor = "white";
+			button.style.backgroundColor = colors[Math.floor(Math.random() * 5)];
 			button.style.padding = "0%";
 		}, 1000);
 	});
 });
-document.addEventListener("keyup", (e) => {});
+document.addEventListener(events[5], (e) => {});
 //Imgs Area
 const allImgs = document.querySelectorAll("img");
+const randomPic = () => {
+	let x = `https://picsum.photos/${Math.floor(Math.random() * 500)}`;
+	console.log(x);
+	return x;
+};
 allImgs.forEach((img) => {
-	img.addEventListener("focus", () => {
-		img.style.width = "60%";
+	img.addEventListener(events[4], () => {
+		img.src = randomPic();
 	});
 });
+
 //Window Area
-window.addEventListener("load", (e) => {
+window.addEventListener(events[6], (e) => {
 	alert(
 		`Welcome To The Fun Bus! Press esc to start the party! Hype up the bus by tapping esc!`
 	);
@@ -49,26 +66,28 @@ window.addEventListener("load", (e) => {
 	document.querySelector(`header`).style.backgroundColor =
 		colors[Math.floor(Math.random() * 5)];
 });
-window.addEventListener("keydown", (e) => {
+window.addEventListener(events[7], (e) => {
 	e.keyCode === 27 ? madness() : null;
 });
 const madness = () => {
-	times++;
-	partyMode(times);
-	allImgs.forEach((img) => {
-		setInterval(() => {
+	timesUserPressesEsc++;
+	partyMode(timesUserPressesEsc);
+
+	setInterval(() => {
+		allImgs.forEach((img) => {
 			x = Math.floor(Math.random() * 88);
 			img.setAttribute("width", `${x}%`);
-			document.querySelector(".home").style.width = `${x}%`;
-			document.querySelector(".home").style.height = `${x}%`;
-			document.querySelector("html").style.backgroundColor = `${
-				colors[Math.floor(Math.random() * 5)]
-			}`;
-			document.querySelector(`header`).style.backgroundColor = `${
-				colors[Math.floor(Math.random() * 5)]
-			}`;
-		}, 1000);
-	});
+			img.src = randomPic();
+		});
+		document.querySelector(".home").style.width = `${x}%`;
+		document.querySelector(".home").style.height = `${x}%`;
+		document.querySelector("html").style.backgroundColor = `${
+			colors[Math.floor(Math.random() * 5)]
+		}`;
+		document.querySelector(`header`).style.backgroundColor = `${
+			colors[Math.floor(Math.random() * 5)]
+		}`;
+	}, 1000);
 };
 //Body
 const body = document.querySelector("body");
